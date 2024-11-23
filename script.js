6f79e714eff309d28dcd6961b0ba5ced3f569408
Ulathi <swiftly.graphics@gmail.com>
2024-11-23 13:19:27 +0200

    added motiondevjs

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 import {
   animate,
-  scroll,
   stagger,
 } from 'https://cdn.jsdelivr.net/npm/motion@11.11.13/+esm';
 
 const mainElement = document.querySelector('.main');
 const downloadButton = document.querySelector('.download');
 
+const container = document.querySelector('.container');
+
 animate(
   mainElement,
-  { opacity: 1, transform: 'translateY(0)' },
+  { opacity: 1 },
   { duration: 0.5, delay: stagger(0.1), ease: 'circInOut' }
+);
+
+animate(
+  container,
+  { transform: 'translateY(0)' },
+  { duration: 0.5, ease: 'circInOut' }
 );
 
 downloadButton.addEventListener('mouseover', () => {
   animate(
     downloadButton,
-    { transform: 'scale(1.05)', backgroundColor: '#fca311' },
+    { transform: 'scale(1.05)', backgroundColor: '#fff' },
     { duration: 0.25, ease: 'circInOut' }
   );
 });
 downloadButton.addEventListener('mouseout', () => {
   animate(
     downloadButton,
-    { transform: 'scale(1)', backgroundColor: '#03045e' },
+    { transform: 'scale(1)', backgroundColor: '#f89920' },
     { duration: 0.25, ease: 'circInOut' }
-  );
-});
-
-downloadButton.addEventListener('click', () => {
-  animate(
-    mainElement,
-    { transform: 'translateY(-250)', opacity: 0 },
-    { duration: 0.5, delay: stagger(0.1), ease: 'circInOut' }
   );
 });
